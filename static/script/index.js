05ce881bc78cfc33051c12dd97f8270880116883
Mykohnguyen <mykohnguyen@gmail.com>
2018-10-10 15:45:43 -0700

    project switches font color

--- static/script/index.js
+++ static/script/index.js
@@ -1,16 +1,16 @@
 
 $('.switch2').click(function(){
-    $('.picture1').attr('src','coding_avatar_home.png'),
-    $('.picture2').attr('src','coding_avatar_classroom.png'),
-    $('.picture3').attr('src','coding_avatar_shop.png'),
+    $('.picture1').attr('src','static/images/coding_avatar_home.png'),
+    $('.picture2').attr('src','static/images/coding_avatar_classroom.png'),
+    $('.picture3').attr('src','static/images/coding_avatar_shop.png'),
     $('.project_info').hide()
     $('.project2_info').show()
 
 }),
 $('.switch').click(function(){
-    $('.picture1').attr('src','momentum_home.png'),
-    $('.picture2').attr('src','momentum_exercises.png'),
-    $('.picture3').attr('src','momentum_stats.png'),
+    $('.picture1').attr('src','static/images/momentum_home.png'),
+    $('.picture2').attr('src','static/images/momentum_exercises.png'),
+    $('.picture3').attr('src','static/images/momentum_stats.png'),
     $('.project_info').show()
     $('.project2_info').hide()
 })
